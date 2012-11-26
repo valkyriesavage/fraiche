62f901733a4a5fa0c175e6e8300402c8c2c7505c
Jonathan Kummerfeld <me@jkk.name>
2012-11-26 11:53:20 -0800

    Started implementing timestep for simulation

--- model/gen_data.py
+++ model/gen_data.py
@@ -27,20 +27,70 @@ def gen_noisy_linear_data(start, slope, noise, timesteps):
   vals = []
   for i in xrange(timesteps):
     mu = start + i * slope
-    vals.append(random.normalvariate(mu, noise)) # Allows sensor to return nonsensical values
-###    vals.append(min(max(0, random.normalvariate(mu, noise)), 100)) # Bounds to [0, 100]
+###    vals.append(random.normalvariate(mu, noise)) # Allows sensor to return nonsensical values
+    vals.append(min(max(0, random.normalvariate(mu, noise)), 100)) # Bounds to [0, 100]
   return vals
 
-#### Factors
-### - rate at which plant uses water
-### - rainfall
-### - watering (i.e. brief period of very heavy rain)
-### - fog / mist
-### - sunlight
-### - wind
-### - soil type
-### - 
+class Simulation:
+  def __init__(self):
+    self.moisture = 0
 
-for noise in [0, 1] + [i * 3 for i in xrange(1, 20)]:
-  vals = gen_noisy_linear_data(90, -2, noise, 20)
-  print ' '.join(["%.2f" % val for val in vals])
+    # Current rate of moisture addition by rain or fog
+    self.rain = 0
+    self.fog = 0
+
+    # World properties that will indirectly influence moisture
+    self.wind = (0, 0) # Current wind strength (North, East)
+    self.storm = [0, 0] # Current storm [severity, time left]
+    self.temperature = [0, 0] # day and night temperatures
+
+    # Time is organised by:
+    #  - Time of day (0 - day, 1 - night)
+    #  - Day of season (0, 89)
+    #  - Season of year (0, 3 - summer, autumn, winter, spring)
+    self.time = [0, 0, 0]
+
+    # System properties
+    self.drainage = 0 # Rate at which water drains from soil
+    self.absorption = 0 # Rate at which plants absorb water
+    self.evaporation = 0 # Base rate of evaporation
+
+  def evaporation(self):
+    # Calculate amount of water that evaporates, based on current conditions
+    # TODO: decide how wind would influence this
+    if self.time[0] == 0:
+      return self.temperature[0] * self.evaporation * 2
+    else:
+      return self.temperature[1] * self.evaporation
+
+  def timestep(self):
+    # Update state, with some randomness
+    self.moisture -= self.drainage
+    self.moisture -= self.absorption
+    self.moisture -= self.evaporation()
+    self.moisture += self.rain
+    self.moisture += self.fog
+
+    # Apply storm
+    if self.storm[1] > 0:
+      pass
+
+    # Increment time
+    self.time[0] += 1
+    if self.time[0] == 2:
+      self.time[0] = 0
+      self.time[1] += 1
+      if self.time[1] == 90:
+        self.time[1] = 0
+        self.time[2] += 1
+        if self.time[2] == 4:
+          self.time[2] = 0
+
+  def water(self):
+    # Simulate a short period of increased moisture
+    pass
+
+if __name__ == '__main__':
+  for noise in [0, 1] + [i * 3 for i in xrange(1, 20)]:
+    vals = gen_noisy_linear_data(90, -2, noise, 20)
+    print ' '.join(["%.2f" % val for val in vals])

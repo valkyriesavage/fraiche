9ec7f7994f910a0534a04445b33d62c65fc3ac40
Jonathan Kummerfeld <me@jkk.name>
2012-12-04 16:47:22 -0800

    Added options to fake sensors

--- fake_sensors.py
+++ fake_sensors.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 
-from model.gen_data import Simulation
+import os, syslog, time, urllib, sys
 
-print "Sensor Manager..."
-print "CS294-84 project based on code from Tinaja labs"
-print "-----------------------------------------------"
-
-import os, syslog, time, urllib
 from server import log_data_file
-
-world = Simulation()
+from model.gen_data import Simulation
 
 ##############################################################
 # the main function
-def mainloop(idleevent):
-  plant_num = '0'
-  state = str(world.get_approximate_state()[0])
-
-  # log the data
-  log_data(plant_num, state)
-  alert_server(plant_num, state)
-
-ZERO_BYTES_FROM = 0
-FILE_END = 2
+def mainloop(world, plants):
+  for i in xrange(plants):
+    plant_num = str(i)
+    state = str(world.get_approximate_state()[0])
+    log_data(plant_num, state)
+    alert_server(plant_num, state)
 
 def log_data(plant_num, sensor_value):
   oclock = time.time()
-  lf = open(log_data_file(plant_num), "w+")
-  lf.seek(ZERO_BYTES_FROM, FILE_END)
+  lf = open(log_data_file(plant_num), "a")
   lf.write(str(oclock) + " " + sensor_value + "\n")
+  lf.close()
 
 def alert_server(plant_num, sensor_data):
   urllib.urlopen('http://127.0.0.1:8888/sensorupdated/' + plant_num + '/' + sensor_data)
 
-# open up the serial port to get data transmitted to xbee
-syslog.syslog("<<<  Starting the Smart Watering Sensor System for H2OIQ  >>>")
+if __name__ == '__main__':
+  world = Simulation()
+  if len(sys.argv) == 1:
+    print "Usage:"
+    print "%s <number of plants> <delay in seconds>" % sys.argv[0]
+    sys.exit(0)
+  plants = int(sys.argv[1])
+  wait_time = float(sys.argv[2])
 
-while True:
-  mainloop(None)
-  time.sleep(2)
+  print "Sensor Manager..."
+  print "CS294-84 project based on code from Tinaja labs"
+  print "-----------------------------------------------"
+  # open up the serial port to get data transmitted to xbee
+  syslog.syslog("<<<  Starting the Smart Watering Sensor System for H2OIQ  >>>")
+
+  while True:
+    mainloop(world, plants)
+    time.sleep(wait_time)
+    world.timestep()

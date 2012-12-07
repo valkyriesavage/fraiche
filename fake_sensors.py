cfeaa4423519435b8170038d6e27dcccecad6451
Jonathan Kummerfeld <me@jkk.name>
2012-12-06 23:18:22 -0800

    Added a command line arg for localhost or not

--- fake_sensors.py
+++ fake_sensors.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-import os, syslog, time, urllib, sys
+import os, syslog, time, urllib, sys, string
 
 from server import log_data_file
 from model.gen_data import Simulation
 
 ##############################################################
 # the main function
-def mainloop(world, plants):
+def mainloop(world, plants, use_localhost):
   for i in xrange(plants):
     plant_num = str(i)
     state = str(world.get_approximate_state()[0])
     log_data(plant_num, state)
-    alert_server(plant_num, state)
+    alert_server(plant_num, state, use_localhost)
 
 def log_data(plant_num, sensor_value):
   oclock = time.time()
@@ -20,25 +20,29 @@ def log_data(plant_num, sensor_value):
   lf.write(str(oclock) + " " + sensor_value + "\n")
   lf.close()
 
-def alert_server(plant_num, sensor_data):
-  urllib.urlopen('http://169.229.63.33:8888/sensorupdated/' + plant_num + '/' + sensor_data)
+def alert_server(plant_num, sensor_data, use_localhost):
+  if use_localhost:
+    urllib.urlopen('http://127.0.0.1:8888/sensorupdated/' + plant_num + '/' + sensor_data)
+  else:
+    urllib.urlopen('http://169.229.63.33:8888/sensorupdated/' + plant_num + '/' + sensor_data)
 
 if __name__ == '__main__':
   world = Simulation()
-  if len(sys.argv) == 1:
+  if len(sys.argv) < 3:
     print "Usage:"
-    print "%s <number of plants> <delay in seconds>" % sys.argv[0]
+    print "%s <number of plants> <delay in seconds> <localhost? default=False" % sys.argv[0]
     sys.exit(0)
   plants = int(sys.argv[1])
   wait_time = float(sys.argv[2])
+  localhost = False
+  if len(sys.argv) == 4:
+    if 't' in string.lower(sys.argv[3]):
+      localhost = True
 
-  print "Sensor Manager..."
-  print "CS294-84 project based on code from Tinaja labs"
-  print "-----------------------------------------------"
   # open up the serial port to get data transmitted to xbee
   syslog.syslog("<<<  Starting the Smart Watering Sensor System for H2OIQ  >>>")
 
   while True:
-    mainloop(world, plants)
+    mainloop(world, plants, localhost)
     time.sleep(wait_time)
     world.timestep()

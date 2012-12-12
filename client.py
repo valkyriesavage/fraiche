51ae24fa7337ccd7bbc766141529ce31c6348486
Valkyrie Savage <valkyrie@eecs.berkeley.edu>
2012-12-11 17:12:45 -0800

    updated to using phantomjs for testing
      * no more splinter, now headless phantom instead
      * note that this means you need to install phantomjs for
        testing

--- client.py
+++ client.py
@@ -1,23 +1,18 @@
-from splinter import Browser
-import random
+import random, subprocess, time
 
 class Client():
+  PHANTOM = "phantomjs"
+  WAITFOR = "testing/waitfor.js"
+
   def __init__(self, plants):
-    self.b = Browser()
-    self.url = "http://169.229.63.33:8888/?plant="
     self.plants = plants
-    self.logging_url = "http://localhost:8888/"
 
   def activate(self):
-    self.b.visit(self.url + str(random.choice(self.plants)))
-    print("url = " + self.url + str(random.choice(self.plants)))
-    #while not self.b.evaluate_script("incoming_data.length") > 2:
-    #  pass
-
-  def log(self, timing):
-    #self.b.visit(self.logging_url + timing)
-    pass
+    plant = random.choice(self.plants)
+    print str(plant) + " start at " + str(time.time())
+    params = [Client.PHANTOM, Client.WAITFOR, str(plant)]
+    subprocess.check_call(params)
 
 if __name__ == '__main__':
-  c = Client()
+  c = Client([0,1,2,3,4])
   c.activate()
--- testing/waitfor.js
+++ testing/waitfor.js
@@ -12,8 +12,8 @@
  * as a callback function.
  * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
  */
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+function waitFor(testFx, onReady, timeOutMillis, whichplant) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 60000, //< Default Max Timout is 60s
         start = new Date().getTime(),
         condition = false,
         interval = setInterval(function() {
@@ -27,7 +27,7 @@ function waitFor(testFx, onReady, timeOutMillis) {
                     phantom.exit(1);
                 } else {
                     // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    console.log(whichplant + "done at " + Math.round((new Date()).getTime() / 1000));
                     typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
                     clearInterval(interval); //< Stop this interval
                 }
@@ -37,22 +37,24 @@ function waitFor(testFx, onReady, timeOutMillis) {
 
 
 var page = require('webpage').create();
+var system = require('system');
+var whichplant = system.args[1];
 
-// Open Twitter on 'sencha' profile and, onPageLoad, do...
-page.open("http://twitter.com/#!/sencha", function (status) {
+// Open plant page of random plant, and, onPageLoad, do...
+page.open("http://169.229.63.33:8888/?plant="+whichplant, function (status) {
     // Check for page load success
     if (status !== "success") {
         console.log("Unable to access network");
     } else {
-        // Wait for 'signin-dropdown' to be visible
+        // Wait for 'incoming_data' to load
         waitFor(function() {
             // Check in the page if a specific element is now visible
             return page.evaluate(function() {
-                return $("#signin-dropdown").is(":visible");
+                return incoming_data.length > 2;
             });
         }, function() {
-           console.log("The sign-in dialog should be visible now.");
+           console.log("The data came");
            phantom.exit();
-        });        
+        }, 60000, whichplant);        
     }
 });
